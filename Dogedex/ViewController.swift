77ff29725532b3b398d4270da05608b1355f46da
Rafael Rollo <rafaelrollo92@gmail.com>
2021-10-28 15:20:16 -0300

    refactor: setup to favor view coding instead of ib usage

--- Dogedex/ViewController.swift
+++ Dogedex/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func loadView() {
+        super.loadView()
+        
+        view.backgroundColor = .systemBackground
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
